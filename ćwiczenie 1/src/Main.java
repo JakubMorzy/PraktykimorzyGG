f761634451ce3d2956f92c22a57006e8f0b3c47c
JakubMorzy <kubamorzy@gmail.com>
2021-02-23 14:39:04 +0100

    Initial commit

---
+++ ćwiczenie 1/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    private static Object String;
+
+    public static void main(String[]args ){
+
+
+//Zadanie 1
+        System.out.println("int");
+        System.out.println("double");
+        System.out.println("char");
+        System.out.println("String");
+        System.out.println("sout");
+//Zadanie 2
+        int intA = 5;
+        int intB = 10;
+        int intC = intA * intA;
+        int intD = intB * intB;
+        int intE = intC + intD;
+        System.out.println(" \nWynik = "+ intE);
+//Zadanie 3
+
+    int intdane = 7;
+    int mianownik = 4;
+    int intpotęga = intdane * intdane;
+     double sqrt = Math.sqrt(3);
+     int intlicznik = (int) (intpotęga + sqrt);
+     double wynik = intlicznik / mianownik;
+        System.out.println(" \nRozwiązanie " + wynik);
+
+//Zadanie 4
+
+        String myStringA = " Jakub ";
+        String myStringB = " Morzy ";
+        String myStringC = myStringA + myStringB;
+        System.out.println(myStringC);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
